51e068c8ec6330e983778e8578954a4806df71c1
rafixadnvn <rafiq.adnan018@gmail.com>
2025-11-15 18:14:03 +0500

    nothing

---
+++ divisible.py
@@ -0,0 +1,10 @@
+print("Enter a Number (Numerator): ")
+numx=int(input())
+print("enter a numberrr(denominator")
+numy=int(input())
+
+if numx%numy==0:
+ print("number is divisible by another")
+else:
+ print("num is not divisble by the other num")
+  
