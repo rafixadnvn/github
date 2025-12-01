767208517caa751e14e602823f7c82de5bb7417e
rafixadnvn <rafiq.adnan018@gmail.com>
2025-12-01 17:10:36 +0500

    fruin

---
+++ decimaltobinary.py
@@ -0,0 +1,10 @@
+num=int(input("enter a decimal number:"))
+binary=""
+if num==0:
+    binary="0"
+else:
+    while num>0:
+        remainder=num%2
+        binary=str(remainder)+binary
+        num=num//2
+print("binay number:",binary)       
