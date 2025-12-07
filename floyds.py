c98aa4659b81c8f87a72a717fbde44ea36f31088
rafixadnvn <rafiq.adnan018@gmail.com>
2025-12-07 19:29:23 +0500

    gods

--- floyds.py
+++ floyds.py
@@ -1,9 +1,9 @@
 rows=int(input("please enter the total number of rows:"))
 number=1
 
-print("floyds triange")
+print("floyds triangel")
 for i in range(1,rows+1):
-    for j in range (1,rows+1):
+    for j in range (1,i+1):
       print(number,end="")
-number=number+1
-print()
+      number=number+1
+    print()
